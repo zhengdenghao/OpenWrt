31228ae018d1f6bc0db28cff806b002f10ac7f73
郑邓浩 <53636413+zhengdenghao@users.noreply.github.com>
2023-01-15 13:42:45 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,5 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git liuran001_packages https://github.com/liuran001/openwrt-packages' >>feeds.conf.default
+echo 'svn co https://github.com/liuran001/openwrt-packages/branches/packages' >>feeds.conf.default
